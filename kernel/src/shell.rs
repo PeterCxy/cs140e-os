e01c8d3eecba71658a6f2dba49731ea0b61fbf74
Peter Cai <peter@typeblog.net>
2018-03-21 09:37:51 +0800

    shell: use a trait for shell commands

--- kernel/src/shell.rs
+++ kernel/src/shell.rs
@@ -2,7 +2,7 @@ use stack_vec::StackVec;
 use console::{kprint, kprintln, CONSOLE};
 use std::io::Write;
 use std::str;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use fat32::vfat::*;
 use fat32::traits::{FileSystem, Entry, Dir};
 use super::FILE_SYSTEM;
@@ -62,69 +62,28 @@ impl<'a> Command<'a> {
     }
 }
 
-// > $ echo $a $b $c
-// > $a $b $c
-// TODO: better implementation of command registry
-fn cmd_echo(cmd: &Command) {
-    for a in cmd.arguments() {
-        kprint!("{} ", a);
-    }
-    kprintln!("");
-}
-
-// Test the panic_fmt impl
-fn cmd_panic(_cmd: &Command) {
-    panic!("Requested panic");
-}
-
-// Test the Atag impl
-fn cmd_atags(_cmd: &Command) {
-    use pi::atags;
-    
-    for tag in atags::Atags::get() {
-        kprintln!("{:#?}", tag);
-    }
-}
-
-// Test heap allocation
-// TODO: better implementation for argument passing and limitations
-fn cmd_heap_test(_cmd: &Command) {
-    let args = _cmd.arguments();
-    if args.len() != 1 {
-        kprintln!("error: incorrect number of arguments for `heap_test`");
-        return;
-    }
-    let num = args[0].parse::<u32>();
-    if let Ok(num) = num {
-        kprintln!("> testing heap allocation...");
-        let mut v = vec![];
-        for i in 0..num {
-            v.push(i);
-        }
-        kprintln!("> {:?}", v);
-        let mut v2 = vec![];
-        for i in 0..num {
-            v2.push(v.pop());
-        }
-        kprintln!("> {:?}", v2);
-        #[cfg(not(test))]
-        kprintln!("{:#?}", super::ALLOCATOR);
-    } else {
-        kprintln!("error: cannot parse {} as number", args[0]);
-    }
-}
+// Registered command list
+static SHELL_CMDS: &'static [&'static ShellCmd] = &[
+    &EchoCmd,
+    &PanicCmd,
+    &AtagsCmd,
+    &HeapTestCmd
+];
 
 // Process a command received from shell
-// TODO: Better implementation
-fn process_command(cmd: Command) {
-    match cmd.path() {
-        "echo" => cmd_echo(&cmd),
-        "panic" => cmd_panic(&cmd),
-        "atags" => cmd_atags(&cmd),
-        "heap_test" => cmd_heap_test(&cmd),
-        //"emm" => FILE_SYSTEM.initialize(),
-        p => kprintln!("unknown command: {}", p)
+fn process_command(pwd: &mut PathBuf, cmd: Command) {
+    let cmd_name = cmd.path();
+
+    // Find the corresponding command
+    // from the registered command list
+    for shell_cmd in SHELL_CMDS {
+        if shell_cmd.name() == cmd.path() {
+            shell_cmd.exec(pwd, &cmd);
+            return;
+        }
     }
+
+    kprintln!("error: unknown command: {}", cmd_name);
 }
 
 /// Starts a shell using `prefix` as the prefix for each line. This function
@@ -136,10 +95,9 @@ pub fn shell(prefix: &str) -> ! {
     kprintln!("");
     kprint!("{}", prefix);
 
-    // TEST FILESYSTEM: REMOVE THIS IN THE FUTURE
-    for entry in FILE_SYSTEM.open(Path::new("/")).unwrap().into_dir().unwrap().entries().unwrap() {
-        kprintln!("{}", entry.name());
-    }
+    // Current working directory
+    let mut pwd = PathBuf::new();
+    pwd.push("/");
 
     // Use a StackVec for storage of command lines
     let mut line_buf = [0u8; 512];
@@ -159,7 +117,7 @@ pub fn shell(prefix: &str) -> ! {
                     let mut cmd_buf = [""; 64];
                     let cmd = Command::parse(line_str, &mut cmd_buf[..]);
                     match cmd {
-                        Ok(cmd) => process_command(cmd),
+                        Ok(cmd) => process_command(&mut pwd, cmd),
                         Err(Error::TooManyArgs) => kprintln!("error: too many arguments"),
                         Err(Error::Empty) => ()
                     }
@@ -190,6 +148,97 @@ pub fn shell(prefix: &str) -> ! {
             // Command not finished and we can still take more characters
             line.push(byte).unwrap();
             CONSOLE.lock().write(&[byte]).unwrap();
+        }
+    }
+}
+
+// Trait for a Shell command
+trait ShellCmd: Sync + Send {
+    // Returns the name (static) of the command
+    fn name(&self) -> &'static str;
+
+    // Called when the command is invoked via shell
+    fn exec(&self, pwd: &mut PathBuf, args: &Command);
+}
+
+// $ echo a b c
+// > a b c
+struct EchoCmd;
+impl ShellCmd for EchoCmd {
+    fn name(&self) -> &'static str {
+        "echo"
+    }
+
+    fn exec(&self, pwd: &mut PathBuf, args: &Command) {
+        for a in args.arguments() {
+            kprint!("{} ", a);
+        }
+        kprintln!("");
+    }
+}
+
+// Trigger kernel panic
+struct PanicCmd;
+impl ShellCmd for PanicCmd {
+    fn name(&self) -> &'static str {
+        "panic"
+    }
+
+    fn exec(&self, pwd: &mut PathBuf, args: &Command) {
+        panic!("Requested panic")
+    }
+}
+
+// Read Atags information from the memory
+struct AtagsCmd;
+impl ShellCmd for AtagsCmd {
+    fn name(&self) -> &'static str {
+        "atags"
+    }
+
+    fn exec(&self, pwd: &mut PathBuf, args: &Command) {
+        use pi::atags;
+    
+        for tag in atags::Atags::get() {
+            kprintln!("{:#?}", tag);
+        }
+    }
+}
+
+// Run a test against the heap
+// $ heap_text x
+// pushes x element into a vector
+// and pop them into another
+// then print the heap allocator status
+struct HeapTestCmd;
+impl ShellCmd for HeapTestCmd {
+    fn name(&self) -> &'static str {
+        "heap_test"
+    }
+
+    fn exec(&self, pwd: &mut PathBuf, args: &Command) {
+        let args = args.arguments();
+        if args.len() != 1 {
+            kprintln!("error: incorrect number of arguments for `heap_test`");
+            return;
+        }
+        let num = args[0].parse::<u32>();
+        if let Ok(num) = num {
+            kprintln!("> testing heap allocation...");
+            let mut v = vec![];
+            for i in 0..num {
+                v.push(i);
+            }
+            kprintln!("> {:?}", v);
+            let mut v2 = vec![];
+            for i in 0..num {
+                v2.push(v.pop());
+            }
+            kprintln!("> {:?}", v2);
+            #[cfg(not(test))]
+            kprintln!("{:#?}", super::ALLOCATOR);
+        } else {
+            kprintln!("error: cannot parse {} as number", args[0]);
         }
     }
 }
